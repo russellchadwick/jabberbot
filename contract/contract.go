14ce164192ec855d3d6e15178001c95b9689a52a
Russell Chadwick <toolshed+github@gmail.com>
2013-10-08 02:34:25 -0700

    Go fmt

--- contract/contract.go
+++ contract/contract.go
@@ -1,11 +1,11 @@
 package contract
 
 type SendChatCommand struct {
-	To		string
-	Text		string
+	To   string
+	Text string
 }
 
 type ChatReceivedEvent struct {
-	From		string
-	Text		string
+	From string
+	Text string
 }
